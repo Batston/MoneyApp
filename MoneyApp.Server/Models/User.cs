ceced9ef0056cee515ca24f7276fab652e37fb81
Minh Phương Vũ <vuminhphuong9029@gmail.com>
2024-12-11 17:53:06 +0700

    Them model user

---
+++ MoneyApp.Server/Models/User.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MoneyApp.Server.Models
+{
+    public class User
+    {
+        [Key]
+        public int UserId {  get; set; }
+        
+        [Required]
+        public string Username { get; set; }
+        
+        [Required]
+        public string Password { get; set; }
+        
+        [Required]
+        public string FullName { get; set; }
+        
+        [EmailAddress(ErrorMessage = "Email không hợp lệ.")]
+        public string Email { get; set; }
+
+        [DisplayFormat(DataFormatString ="{0:dd-MM-yyyy}")]
+        public DateTime CreateDate { get; set; }
+    }
+}
