0257c9d298c2fcf3c082b5bca5d64fad466b8069
Minh Phương Vũ <vuminhphuong9029@gmail.com>
2024-12-13 10:50:38 +0700

    add delele wallet

--- MoneyApp.Server/Controllers/WalletController.cs
+++ MoneyApp.Server/Controllers/WalletController.cs
@@ -1,7 +1,10 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using MoneyApp.Server.Data;
+using MoneyApp.Server.Models;
+using System.Security.Claims;
 
 namespace MoneyApp.Server.Controllers
 {
@@ -17,6 +20,7 @@ namespace MoneyApp.Server.Controllers
         }
 
         [HttpGet("{id}")]
+        [Authorize]
         public async Task<IActionResult> GetWallet(int id)
         {
             var result = await _context.Wallets.Where(w => w.UserId == id).ToListAsync();
@@ -25,6 +29,29 @@ namespace MoneyApp.Server.Controllers
                 return NotFound("Không tìm thấy ví của người dùng này!");
                 
             return Ok(result);
+        }
+
+        [HttpDelete("{id}")]
+        [Authorize]
+        public async Task<IActionResult> DeleteWallet(int id)
+        {
+            var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+            if (userId == null)
+                return Unauthorized("Nguời dùng không hợp lệ!");
+
+            var wallet = await _context.Wallets.FindAsync(id);
+
+            if (wallet == null)
+                return NotFound("Không tìm thấy ví của người dùng này");
+
+            if (wallet.UserId != int.Parse(userId))
+                return StatusCode(403 ,new {Message = "Người dùng không có quyền thực hiện yều cầu này!"});
+
+            _context.Wallets.Remove(wallet);
+            await _context.SaveChangesAsync();
+
+            return Ok(new { Message = "Xóa ví thành công!" }); 
         }
     }
 }
