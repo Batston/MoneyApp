654a7f9c5012825603b632757ed6f5ff5ff2adf5
Minh Phương Vũ <vuminhphuong9029@gmail.com>
2024-12-16 14:59:34 +0700

    add get categories list

---
+++ MoneyApp.Server/Controllers/CategoryController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+using MoneyApp.Server.Data;
+
+namespace MoneyApp.Server.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        public readonly DBContext _context;
+        public CategoryController(DBContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        [Authorize]
+        public async Task<IActionResult> Get()
+        {
+            var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+            if (userId == null)
+                return Unauthorized(new { Message = "Người dùng không hợp lệ!" });
+
+            var category = await _context.Categories.ToListAsync();
+
+            if (category == null)
+                return NotFound(new { Message = "Không tìm thấy danh mục!" });
+
+            return Ok(category);
+        }
+    }
+}
