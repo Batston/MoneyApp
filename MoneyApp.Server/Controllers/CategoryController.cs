6121f93b90b60f1ffcca7a6c8bdee4ffe0d807d1
Minh Phương Vũ <vuminhphuong9029@gmail.com>
2024-12-16 15:09:30 +0700

    get category by type

--- MoneyApp.Server/Controllers/CategoryController.cs
+++ MoneyApp.Server/Controllers/CategoryController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Security.Claims;
 using MoneyApp.Server.Data;
+using MoneyApp.Server.Models;
 
 namespace MoneyApp.Server.Controllers
 {
@@ -17,9 +18,45 @@ namespace MoneyApp.Server.Controllers
             _context = context;
         }
 
-        [HttpGet]
+        [HttpGet("{id}")]
         [Authorize]
-        public async Task<IActionResult> Get()
+        public async Task<IActionResult> Get(int id)
+        {
+            var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+            if (userId == null)
+                return Unauthorized(new { Message = "Người dùng không hợp lệ!" });
+
+            var category = new List<Category> { };
+
+            if (id == 0) //all
+            {
+                category = await _context.Categories.ToListAsync();
+
+                if (category == null)
+                    return NotFound(new { Message = "Không tìm thấy danh mục!" });
+            }
+            else if (id == 1) //chi
+            {
+                category = await _context.Categories.Where(c => c.Type == "Chi").ToListAsync();
+
+                if (category == null)
+                    return NotFound(new { Message = "Không tìm thấy danh mục chi!" });
+            }
+            else if (id == 2) //thu
+            {
+                category = await _context.Categories.Where(c => c.Type == "Thu").ToListAsync();
+
+                if (category == null)
+                    return NotFound(new { Message = "Không tìm thấy danh mục Thu!" });
+            }
+
+            return Ok(category);
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> AddCategory( )
         {
             var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
 
